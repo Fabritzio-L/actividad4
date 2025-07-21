68af65cd4633cd3f9c924b4e08f601f18fd6e26f
fabri <fabritzioalejandrol@gmail.com>
2025-07-21 12:07:09 -0600

    imprimir los datos de los impuestos

--- ejercicio2.py
+++ ejercicio2.py
@@ -3,13 +3,18 @@ dependientes = int(input("Ingrese el numero de dependientes: "))
 if ingreso_anual < 40000 and dependientes >2:
     print("No paga impuestos.")
 else:
-    if  0>= ingreso_anual <=30000:
+    if  ingreso_anual <=30000:
         impuestos= ingreso_anual * 0.05
     elif ingreso_anual <= 60000:
         impuestos = ingreso_anual * 0.1
     elif ingreso_anual <= 100000:
         impuestos= ingreso_anual * 0.15
-    elif impuestos > 100000:
-        impuestos = ingreso_anual * 0.2
     else:
-        print("Dato no valido")
+        impuestos = ingreso_anual * 0.2
+    deducccion = dependientes * 1000
+    impuesto_final = impuestos - deducccion
+    if impuesto_final <0:
+        impuesto_final=0
+    print(f"Su impuesto inicial era de: Q{impuestos}")
+    print(f"Su deducción por dependientes es de: Q{deducccion}")
+    print(f"Su iimpuesto final a pagar es de : Q{impuesto_final}")
