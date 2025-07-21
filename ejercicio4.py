3f4502bc1468b2d7b8332c74fa3f62735a312ab6
fabri <fabritzioalejandrol@gmail.com>
2025-07-21 14:27:35 -0600

    Mostrar el precio del producto y agregarlo al carrito

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,4 +1,19 @@
-productos = {"Pizza": 55.00, "Hamburguesa":35.99, "Pastel":17.50}
+productos = {"pizza": 55.00, "hamburguesa":35.99, "pastel":17.50}
 carrito =[]
 for i in productos:
     print(f"-{i}")
+while True:
+    solicitar_producto = input("\nIngrese el nombre del producto para consultar precio (o 'fin' para terminar): ").strip().lower()
+    if solicitar_producto in productos:
+        precio = productos[solicitar_producto]
+        print(f"El precio de {solicitar_producto} es: Q{precio}")
+        agregar_producto = input("¿Desea agregar el producto al carrito? (si o no)").lower()
+        if agregar_producto in ("si","sí"):
+            carrito.append(precio)
+            print(f"{agregar_producto} agregado al carrito")
+        elif agregar_producto in ("no"):
+            print("Producto no agregado")
+        else:
+            print("Ingrese una respuesta valida")
+    else:
+        print("Producto no encontrado.")
