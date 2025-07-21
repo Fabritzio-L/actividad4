a6cb7905a26b9dc57fc00cc395506c3efec2a203
fabri <fabritzioalejandrol@gmail.com>
2025-07-21 12:29:28 -0600

    verificar si existe usuario y su contraseña

--- ejercicio3.py
+++ ejercicio3.py
@@ -5,3 +5,14 @@ acceso = False
 while intentos < 3:
     usuario =input("Ingrese su nombre de usuario: ")
     contraseña = input("Ingrese su contraseña: ")
+    usuario_correcto= False
+    for i in range(len(usuarios)):
+        if usuarios[i] == usuario:
+            usuario_correcto = True
+            if contraseñas[i] == contraseña:
+                print("Acceso concedido")
+                acceso = True
+            else:
+                intentos += 1
+                print(f"Contraseña incorrecta, le quedan {intentos} intentos.")
+            break
