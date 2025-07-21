b57a4d0d0513238c942337bdc513674f6c2e6c27
fabri <fabritzioalejandrol@gmail.com>
2025-07-21 12:17:41 -0600

    definir usuarios y contraseñas correctas

---
+++ ejercicio3.py
@@ -0,0 +1,7 @@
+usuarios = ["usuario1","usuario2","usuario3"]
+contraseñas = ["123","abc","123abc"]
+intentos = 0
+acceso = False
+while intentos < 3:
+    usuario =input("Ingrese su nombre de usuario: ")
+    contraseña = input("Ingrese su contraseña: ")
