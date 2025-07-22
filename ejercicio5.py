0a75d9b9e8680875938c46589846ef86e4adf455
fabri <fabritzioalejandrol@gmail.com>
2025-07-21 18:31:22 -0600

    Dia segun la formula zeller

--- ejercicio5.py
+++ ejercicio5.py
@@ -5,11 +5,19 @@ bisiesto = (año % 4 == 0 and año % 100 != 0) or (año % 400 == 0)
 dias_mes = [31,28,31,30,31,30,31,31,30,31,30,31]
 if bisiesto:
     dias_mes[1]=29
-if 1 <dia < dias_mes[mes -1]:
+if 1 <dia > dias_mes[mes -1]:
     print("Dia invalido")
-elif 1 < mes < 12:
+elif 1 < mes > 12:
     print("Mes invalido")
 else:
     q = dia
     m = mes
     a= año
+    if m < 3:
+        m +=12
+        a -=1
+    K = a %100
+    J = a //100
+    h = (q + (13 * (m + 1)) // 5 + K + (K // 4) + (J // 4) + 5 * J) % 7
+    dias = ["Sabado","Domingo","Lunes","Martes","Miercoles","Jueves","Viernes"]
+    print(f"El {dia}/{mes}/{año} es el día {dias[h]}")
