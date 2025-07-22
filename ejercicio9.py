72e2009eec8585fc818d36bac2883300f7ebf964
fabri <fabritzioalejandrol@gmail.com>
2025-07-21 22:19:57 -0600

    Datos a pedir

---
+++ ejercicio9.py
@@ -0,0 +1,4 @@
+dia= input("¿Que dia de la semana es?: ").lower()
+edad= int(input("Ingrese su edad: "))
+estudiante= input("¿Usted es estudiante?: ")
+clasificacion = int(input("Ingrese la clasificación de la pelicula: "))
