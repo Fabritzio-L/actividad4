d4fdd95a563c51c12674653c73c0fb5d2fb9e429
fabri <fabritzioalejandrol@gmail.com>
2025-07-21 18:36:22 -0600

    Datos pedidos

---
+++ ejercicio6.py
@@ -0,0 +1,4 @@
+peso_paquete= float(input("Ingrese el peso del paquete: "))
+distancia = float(input("Ingrese la distancia en km del envio: "))
+urgencia = input("¿El envio es de urgencia (si/no)?: ").lower()
+tamaño = input("Ingrese el tamaño del paquete pequeño/mediano/grande: ").lower()
