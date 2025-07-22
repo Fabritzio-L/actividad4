5acb3c6b318e40c95d4e72429479fb15b2af7c85
fabri <fabritzioalejandrol@gmail.com>
2025-07-21 23:09:57 -0600

    Comprobar que fecha es mayor

--- ejercicio10.py
+++ ejercicio10.py
@@ -6,3 +6,23 @@ print("Fecha 2")
 dia_2 = int(input("Ingrese el dia: "))
 mes_2 = int(input("Ingrese el mes: "))
 año_2 = int(input("Ingrese el año: "))
+if año_1 > año_2:
+    mayor = "Fecha 1 es mayor"
+elif año_1 < año_2:
+    mayor = "Fecha 2 es mayor"
+else:
+    if mes_1 > mes_2:
+        mayor = "Fecha 1 es mayor"
+    elif mes_1 < mes_2:
+        mayor= "Fecha 2 es mayor"
+    else:
+        if dia_1 > dia_2:
+            mayor = "Fecha 1 es mayor"
+        elif dia_1 < dia_2:
+            mayor = "Fecha 2 es mayor"
+        else:
+            mayor= "Las fechas son iguales"
+if mes_1 == mes_2 and año_1 == año_2:
+    mismo_año_mes = "Estan en el mismo año y mes"
+else:
+    mismo_año_mes= "Estan en años o meses distintos"
