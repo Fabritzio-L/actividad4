883ffbc5336e77d77b1b587ab659652a98dc2f4f
fabri <fabritzioalejandrol@gmail.com>
2025-07-21 22:17:35 -0600

    Ultimos posibles destinos

--- ejercicio8.py
+++ ejercicio8.py
@@ -1,5 +1,11 @@
 origen = input("¿Desde donde se encuentra?:").lower()
 destino = input("¿A donde se dirige?: ").lower()
+if (origen == "norte" and destino == "sur") or (origen == "sur" and destino == "norte"):
+    print("Dirijase recto al norte" if destino == "norte" else "Dirijase recto al sur")
+elif (origen == "este" and destino == "oeste") or (origen == "oeste" and destino == "este"):
+    print("Dirijase recto al este" if destino == "este" else "Dirijase recto al oeste")
+elif origen == destino:
+    print("Ya se encuentra en su destino")
 if origen == "norte":
     if destino == "este":
         print("Dirijase al noreste")
